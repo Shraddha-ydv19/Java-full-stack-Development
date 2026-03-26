cb96a163193499de871846f9c27ea9cdd90ce862
2k22.cscys.2211334@gmail.com <2k22.cscys.2211334@gmail.com>
2026-03-26 16:34:46 +0530

    updated submodule

--- AdvancedJavaa/Swing.java
+++ AdvancedJavaa/Swing.java
@@ -1,6 +1,8 @@
 package AdvancedJavaa;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.JFileChooser.*;
 
 public class Swing {
     public static void main(String[] args) {
@@ -11,7 +13,7 @@ public class Swing {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // JButton button = new JButton("click me");
+        // JButton button = new JButton("Click Me");
         // button.setBounds(120, 100, 150, 150);
 
         // frame.add(button);
@@ -34,25 +36,47 @@ public class Swing {
        // JOptionPane.showMessageDialog(frame, "welcome to java swing" , "ALERT", JOptionPane.WARNING_MESSAGE);
 
 
-       JMenuBar menuBar = new JMenuBar();
-       JMenu menu,subMenu;
+    //    JMenuBar menuBar = new JMenuBar();
+    //    JMenu menu,subMenu;
        
-       JMenuItem l1 ,l2,l3,l4;
-         menu = new JMenu("File");
-            subMenu = new JMenu("New");
-           l1= new JMenuItem("project");
-              l2= new JMenuItem("file");
-                menu.add(l1);
-                menu.add(l2);
+    //    JMenuItem l1 ,l2,l3,l4;
+    //      menu = new JMenu("File");
+    //         subMenu = new JMenu("New");
+    //        l1= new JMenuItem("project");
+    //           l2= new JMenuItem("file");
+    //             menu.add(l1);
+    //             menu.add(l2);
 
-                menu.add(subMenu);
-                l3 = new JMenuItem("open");
-                l4 = new JMenuItem("save"); 
-                subMenu.add(l3);
-                subMenu.add(l4);
-                menu.add(subMenu);
-                menuBar.add(menu);
-              frame.setJMenuBar(menuBar);
+    //             menu.add(subMenu);
+    //             l3 = new JMenuItem("open");
+    //             l4 = new JMenuItem("save"); 
+    //             subMenu.add(l3);
+    //             subMenu.add(l4);
+    //             menu.add(subMenu);
+    //             menuBar.add(menu);
+    //           frame.setJMenuBar(menuBar);
+
+
+
+    //Tree......
+    // DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+    // DefaultMutableTreeNode child1 =new DefaultMutableTreeNode("Child 1");
+    // DefaultMutableTreeNode child2 =new DefaultMutableTreeNode("Child 2");
+    // root.add(child1);
+    // root.add(child2);
+    // DefaultMutableTreeNode grandChild1 = new DefaultMutableTreeNode("Grandchild 1");
+    // DefaultMutableTreeNode grandChild2 = new DefaultMutableTreeNode("Grandchild 2");
+    // child1.add(grandChild1);
+    // child1.add(grandChild2);
+    // JTree tree = new JTree(root);
+    // tree.setBounds(50, 50, 200, 300);
+    // frame.add(tree);
+     
+
+    //FileChooser......
+    JFileChooser fileChooser = new JFileChooser();
+    fileChooser.showSaveDialog(null);
+
         frame.setVisible(true);
     }
 }
